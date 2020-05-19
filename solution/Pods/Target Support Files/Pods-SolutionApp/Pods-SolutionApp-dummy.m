69e6615842856097c89f2d2ab201fdfbe2934f02
Alex Muramoto <amuramoto@apache.org>
2020-05-18 17:24:32 -0700

    Updates gitignore to remove Pods/

--- solution/Pods/Target Support Files/Pods-SolutionApp/Pods-SolutionApp-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_SolutionApp : NSObject
-@end
-@implementation PodsDummy_Pods_SolutionApp
-@end
--- starter/Pods/Target Support Files/Pods-StarterApp/Pods-StarterApp-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_StarterApp : NSObject
-@end
-@implementation PodsDummy_Pods_StarterApp
-@end
