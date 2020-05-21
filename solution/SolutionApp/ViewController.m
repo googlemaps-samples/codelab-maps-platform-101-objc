918638a35d6679c34b006455bbf20f101128d0b3
Alex Muramoto <amuramoto@apache.org>
2020-05-20 21:55:43 -0700

    small implementation changes

--- solution/SolutionApp/ViewController.m
+++ solution/SolutionApp/ViewController.m
@@ -40,34 +40,31 @@
   [super viewDidLoad];
   
   // Add a single marker with a custom icon
-  CLLocationCoordinate2D mapCenter = CLLocationCoordinate2DMake(-33.86, 151.20);
+  CLLocationCoordinate2D mapCenter = CLLocationCoordinate2DMake(_mapView.camera.target.latitude,
+                                                                _mapView.camera.target.longitude);
   GMSMarker *marker = [GMSMarker markerWithPosition:mapCenter];
   marker.icon = [UIImage imageNamed:@"custom_pin.png"];
+  marker.map = _mapView;
+  NSArray<GMSMarker *> *markerArray = [LocationGenerator generateMarkersNear:mapCenter count:100];
   
   // Set up the cluster manager with a supplied icon generator and renderer.
-  id<GMUClusterAlgorithm> algorithm =
-  [[GMUNonHierarchicalDistanceBasedAlgorithm alloc] init];
-  id<GMUClusterIconGenerator> clusterIconGenerator =
-  [[GMUDefaultClusterIconGenerator alloc] init];
-  id<GMUClusterRenderer> renderer =
-  [[GMUDefaultClusterRenderer alloc] initWithMapView:_mapView
-                                clusterIconGenerator:clusterIconGenerator];
+  id<GMUClusterAlgorithm> algorithm = [[GMUNonHierarchicalDistanceBasedAlgorithm alloc] init];
+  id<GMUClusterIconGenerator> clusterIconGenerator = [[GMUDefaultClusterIconGenerator alloc] init];
+  id<GMUClusterRenderer> renderer = [[GMUDefaultClusterRenderer alloc] initWithMapView:_mapView
+                                                                  clusterIconGenerator:clusterIconGenerator];
   _clusterManager = [[GMUClusterManager alloc] initWithMap:_mapView
                                                  algorithm:algorithm
                                                   renderer:renderer];
   
-  // Generate and add random items to the cluster manager.
-  NSArray<GMSMarker *> *markerArray = [LocationGenerator generateMarkersNear:mapCenter count:100];
+  // Add markers to the cluster manager.
   [_clusterManager addItems:markerArray];
   // Render clusters from items on the map
   [_clusterManager cluster];
 }
 
 - (BOOL)mapView:(GMSMapView *)mapView didTapMarker:(GMSMarker *)marker {
-  
-  CLLocationCoordinate2D position = marker.position;
-  [_mapView animateToLocation:position];
   _circ.map = nil;
+  [_mapView animateToLocation:marker.position];
   
   if ([marker.userData conformsToProtocol:@protocol(GMUCluster)]) {
     [_mapView animateToZoom:_mapView.camera.zoom +1];
@@ -75,7 +72,7 @@
   }
   
   // render the radius circle
-  _circ = [GMSCircle circleWithPosition:position radius:800];
+  _circ = [GMSCircle circleWithPosition:marker.position radius:800];
   _circ.fillColor = [UIColor colorWithRed: 0.67 green: 0.67 blue: 0.67 alpha: 0.5];
   _circ.map = _mapView;
   return NO;
